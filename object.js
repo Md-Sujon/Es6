ace2fba1cc4b24dbac9d2846a6a10ab61b75daa4
Md-Sujon <mdsujon829562@gmail.com>
2021-11-18 15:51:45 +0600

    oop

---
+++ object.js
@@ -0,0 +1,13 @@
+class Student{
+      constructor(sID, sName){
+          this.id = sID;
+          this.name = sName;
+          this.School = "BankerHat School"
+      }
+}
+
+const Student1 = new Student(12, 'Sujon');
+const Student2 = new Student(15, 'Smita');
+
+console.log(Student1, Student2);
+ 
