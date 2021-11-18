dc48a13bc9e1d4721204b7fc6022b19b5e9ba9d3
Md-Sujon <mdsujon829562@gmail.com>
2021-11-18 11:31:36 +0600

    es6

---
+++ tempalteString.js
@@ -0,0 +1,7 @@
+const line = `this is sujon
+hi sujon
+ki koro sujon`
+
+
+const line2 = `sujon ${69+70+20} ${line} i miss you`
+console.log(line2);
---
+++ threeDoits.js
@@ -0,0 +1,17 @@
+const ages = [45,78,45,152,54,251];
+const ages2 = [30,52,32,12,36,21,45,23];
+const ages3 = [78,89,5744,5874];
+const allAges = ages.concat(ages2).concat([5455244]).concat(ages3);
+const allAges2 = [...ages, ...ages2,6363, ...ages3];
+console.log(allAges2);
+
+
+const sujon = 450;
+const smita = 1100;
+const sumi = 800;
+
+const array = [800,600,6450];
+
+
+const maximum = Math.max(...array);
+console.log(maximum);
